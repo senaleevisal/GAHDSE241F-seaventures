0f800370260408dd1120ce51ae17f24f6ff9d51e
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2025-02-23 11:16:49 +0530

    Delete client directory

--- client/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3wEuNunrPWAQXXAf5LTzPKIOzUmhSKDL9FC4obS8sTk=\"\n}"
--- client/src/app/components/atom/FooterText.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FooterText = ({ footertext }: { footertext: string }) => {
-  return <p className="text-gray-400">{footertext}</p>;
-};
-
-export default FooterText;
